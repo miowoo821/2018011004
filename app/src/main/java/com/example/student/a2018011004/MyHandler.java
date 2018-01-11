d273d18cb1a54fd5ab8d88e8c7458972de390968
miowoo <miowoo821@gmail.com>
2018-01-11 14:03:43 +0800

    抓敘述

--- app/src/main/java/com/example/student/a2018011004/MyHandler.java
+++ app/src/main/java/com/example/student/a2018011004/MyHandler.java
@@ -18,7 +18,7 @@ public class MyHandler extends DefaultHandler{
     boolean istitle=false;
     boolean isitem=false;
     boolean islink=false;//準備抓連結
-    //boolean isdescription=false;刪除
+    boolean isdescription=false;
     StringBuilder linkSB=new StringBuilder();//弄一個大字串物件給連結存放用,不能像title直接抓，因為會抓到沒有item的連結，title也會，但用isitem處理，兩個方法疑似都行
    // StringBuilder descriptionSB=new StringBuilder();刪除
 
@@ -30,41 +30,60 @@ mobile01newitem item;
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        if(qName.equals("title")){
-            istitle=true;
-        }
-        if(qName.equals("item")){
-            isitem=true;
-            item= new mobile01newitem();//item的頭就新增物件
+
+        //用switch取代if
+        switch (qName){
+            case "title":
+                istitle=true;
+                break;
+            case "item":
+                isitem=true;
+                item=new mobile01newitem();//新增物件
+                break;
+            case "link":
+                islink=true;
+                break;
+            case "description":
+                isdescription=true;
+                break;
         }
 
-        if (qName.equals("link")){
-            islink=true;
-        }
-//        if (qName.equals("description")){
-//            //isdescription=true;
-//        }刪除
+//   刪除     if(qName.equals("title")){            istitle=true;        }
+//     刪除   if(qName.equals("item")){            isitem=true;            item= new mobile01newitem();//item的頭就新增物件        }
+//     刪除   if (qName.equals("link")){            islink=true;        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
-       if(qName.equals("title")){
-           istitle=false;
-       }
-        if(qName.equals("item")){
-            isitem=false;
-            newitems.add(item);//把剛剛新增的物件丟進去
+        switch (qName){
+            case "title":
+                istitle=false;
+                break;
+            case "item":
+                isitem=false;
+                newitems.add(item);//把剛剛新增的物件丟進去
+                break;
+            case "link":
+                islink=false;
+                if(isitem){
+                    item.link=linkSB.toString();
+                    linkSB=new StringBuilder();
+                }
+                break;
+            case "description":
+                isdescription=true;
+                break;
         }
-        if(qName.equals("link")){
-            islink=false;
-            if(isitem){//在item等於true(原始碼有item標籤)的時候才執行放入link的動作
-                item.link=linkSB.toString();//把mobile01的物件塞入item
-
-//            links.add(linkSB.toString());
-//            linkSB=new StringBuilder();//清除linkSB的內容
-            }
-        }
+//    刪除    if(qName.equals("title")){           istitle=false;       }
+//        if(qName.equals("item")){            isitem=false;            newitems.add(item);//把剛剛新增的物件丟進去        }
+//        if(qName.equals("link")){            islink=false;
+//            if(isitem){//在item等於true(原始碼有item標籤)的時候才執行放入link的動作
+//                item.link=linkSB.toString();//把mobile01的物件塞入item
+////            links.add(linkSB.toString());
+////            linkSB=new StringBuilder();//清除linkSB的內容
+//            }
+//        }
 //        if (qName.equals("description")){
 //            isdescription=false;
 //            if(isitem){//在item等於true(原始碼有item標籤)的時候才執行放入description的動作
@@ -86,10 +105,10 @@ mobile01newitem item;
             Log.d("NET",new String(ch,start,length));
             linkSB.append(new String(ch,start,length));
         }
-//        if (isdescription && isitem){
-//            Log.d("NET",new String(ch,start,length));
-//            descriptionSB.append(new String(ch,start,length));
-//        }刪除
+        if (isdescription && isitem){
+            Log.d("NET",new String(ch,start,length));
+            item.description=new String(ch,start,length);
+        }
 
     }
 
